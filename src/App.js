345aa4e2144be64107b521316c7755375c9871bd
ankit9327 <nagar09ankit@gmail.com>
2021-12-12 21:52:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import DataFetchingOne from './Components/DataFetchingOne'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+     <DataFetchingOne/>
+    </div>
+  );
+}
